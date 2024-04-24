c59890c381d8ddbc0fee49f7e6d88df643f8f218
Gideon <65892781+gideon-pol@users.noreply.github.com>
2024-04-24 16:23:03 +0200

    Fast dvfs

--- simulationcontrol/run.py
+++ simulationcontrol/run.py
@@ -414,6 +414,42 @@ def multi_threading_test():
                     print("Big L")
 
 
+def dvfs_symmetric_fast_test():
+    for benchmark in (
+        "parsec-blackscholes",
+        "parsec-bodytrack",
+        "parsec-canneal",
+        "parsec-dedup",
+        # "parsec-fluidanimate",
+        # "parsec-streamcluster",
+        # "parsec-swaptions",
+        # "parsec-x264",
+        #'splash2-barnes',
+        #'splash2-fmm',
+        #'splash2-ocean.cont',
+        #'splash2-ocean.ncont',
+        #'splash2-radiosity',
+        #'splash2-raytrace',
+        #'splash2-water.nsq',
+        #'splash2-water.sp',
+        #'splash2-cholesky',
+        #'splash2-fft',
+        #'splash2-lu.cont',
+        #'splash2-lu.ncont',
+        #'splash2-radix',
+    ):
+        min_parallelism = get_feasible_parallelisms(benchmark)[0]
+        max_parallelism = get_feasible_parallelisms(benchmark)[-1]
+        for cores in [2, 3, 4]:
+            for freq in (2, 4):
+                try:
+                    run(
+                        ["{:.1f}GHz".format(freq), "maxFreq", "fastDVFS"],
+                        get_instance(benchmark, cores, input_set="simsmall"),
+                    )
+                except Infeasible:
+                    print("Big L")
+
 def dvfs_test():
     for benchmark in (
         # "parsec-blackscholes",
@@ -585,7 +621,8 @@ def main():
     # multi_threading_test()
     # migration_test()
     # multi_program_test()
-    dvfs_test()
+    # dvfs_test()
+    dvfs_symmetric_fast_test()
 
 
 if __name__ == "__main__":
